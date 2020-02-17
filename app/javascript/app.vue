65a512b89b3ecbc69d46cbbd46568c65da1958a3
Guilherme Lourenço <guilhermelourenco1997@gmail.com>
2020-02-17 12:44:13 -0300

    first commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ["original_lists"],
+  data: function() {
+    return {
+      message: "Hellooooo"
+    };
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
